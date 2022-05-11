80655d012f9a68ed8006c92ff67ba08b10cb6fda
Lukas <lukas.kveraga@gmail.com>
2022-05-11 16:33:02 +0300

    feature: initial project setup

---
+++ src/constants/routes.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Home from '../pages/home';
+import NotFound from '../pages/notFound';
+
+export const ROUTE_NAMES = {
+    NOT_FOUND: '*',
+    DEFAULT: '/',
+    LOGIN: '/login',
+};
+
+export const ROUTE_REDERECT = {
+    NOT_FOUND: '*',
+    DEFAULT: '/',
+    LOGIN: '/login/',
+};
+
+export const ROUTES = [
+    {
+        path: ROUTE_NAMES.DEFAULT,
+        component: <Home />,
+        exact: true,
+    },
+    {
+        path: ROUTE_NAMES.NOT_FOUND,
+        component: <NotFound />,
+        exact: true,
+    },
+];
---
+++ src/pages/notFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function NotFound() {
+    return (
+        <div className="bg-red-400 h-5">
+            <h1>Ah 💩, page not found!</h1>
+        </div>
+    );
+}
